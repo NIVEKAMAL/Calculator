68520d1142c74c23927f7f9d1c06d484f98b30c8
Kamal SELVAM <kamalbowselvam@gmail.com>
2017-07-24 19:20:21 +0200

    Update sine1.py

--- sine1.py
+++ sine1.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-Fs = 8000
 f = 5
-sample = 8000
-x = np.arange(sample)
-y = np.sin(2 * np.pi * f * x / Fs)
+Ampli = 2
+
+x = np.linspace(0,1,100)
+y = Ampli * np.sin(2 * np.pi * f * x )
 plt.plot(x, y)
 plt.xlabel('voltage(V)')
 plt.ylabel('sample(n)')
